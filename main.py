3adf77570de32b713c1be7a368c090730197bb20
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2022-02-22 09:45:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,16 @@ global list
 list =[]
 
 
-def linstern1():
-    global amounttimereal
-    with Listener(on_click=on_click2) as listener1:
-        Timer(amounttimereal, listener1.stop).start()
-        listener1.join()
-
-
-def on_click2(x, y, button, pressed):
-    if pressed:
-        exicuting_code()
-
-
 def on_click(x, y, button, pressed):
     if pressed:
         global mousepos
-        mousepos.append("Mouse clicked")
-        print("Mouse clicked")
+        mousepos.append
+        exicuting_code()
 
 
 def linstern():
     global amounttime
     with Listener(on_click=on_click) as listener:
-        Timer(amounttime, listener.stop).start()
         listener.join()
 
 
@@ -47,30 +34,23 @@ def actionstocode():
     global mousepos
     global list
     list = mousepos
-    print (list)
     length_of_list = len(list)-1
     while (length_of_list >= 0):
-        y=0
-        print("code")
         x = list[length_of_list]
-        print(x)
-        if type(x) == float == True or type(x) == int == True:
-            list[length_of_list] = time.sleep(float(x))
-            y=1
-        if y==0:
+        print(length_of_list)
+        try:
+            x[0] == "M" == True
+        except:
+            list[length_of_list] = "time.sleep("+str(float(x))+")"
+        else:
             if x[0] == "M":
                 list[length_of_list] = pyautogui.click()
-                y = 1
-        if y == 0:
-            if x[0] < 1000:
-                list[length_of_list] = pyautogui.moveTo(list[length_of_list][0],list[length_of_list][1])
-                y = 1
-        if y == 0:
-            if x[0] >= 1000:
-                list[length_of_list] = pyautogui.moveTo(list[length_of_list][0],list[length_of_list][1])
-                y = 1
+            elif x[0] < 1000:
+                list[length_of_list] = "pyautogui.moveTo("+str(list[length_of_list][0])+","+str(list[length_of_list][1])+")"
+            elif x[0] >= 1000:
+                list[length_of_list] ="pyautogui.moveTo("+str(list[length_of_list][0])+","+str(list[length_of_list][1])+")"
         length_of_list-=1
-    making_code_list()
+    linstern()
 
 
 def position():
@@ -96,35 +76,13 @@ def position():
     actionstocode()
 
 
-def making_code_list():
-    global list
-    global code
-    x = 0
-    for x in range(0, len(list)):
-        code = lambda x: x + 1
-        x += 1
-    linstern1()
-
-
 def exicuting_code():
     global list
     code = list
     x=0
     print("exicuting")
-    while x <= len(list):
-        print(code[x-1])
+    while x < len(list):
+        exec(code[x])
         x += 1
 
-
-if __name__=='__main__':
-    p1 = Process(target = position)
-    p1.start()
-    p2 = Process(target = linstern)
-    p2.start()
-
-
-
-
-# Ex. of printing code
-#x = pyautogui.moveTo(x=0, y=0)
-#x
+position()
